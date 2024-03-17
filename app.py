40c335b4584a1ba35c637aa2fc8c522ace0427f8
madhav <sai.madhav@bandhoo.com>
2024-03-17 12:09:38 +0530

    hyperopt for xgboost done

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import imp
+from unicodedata import numeric
+from catboost import CatBoostClassifier
 import wget
 import pandas as pd
 import streamlit as st
@@ -15,7 +17,103 @@ from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
+
+from hyperopt import hp,fmin,tpe,STATUS_OK,Trials
+from utils import *
+
+
+
+# ********Constants****************************************************
+numeric_cols = [
+   'age', 
+   'dailyrate',
+   'distancefromhome',
+   'employeenumber',
+   'hourlyrate',
+   'monthlyincome',
+   'monthlyrate',
+   'numcompaniesworked',
+   'percentsalaryhike',
+   'totalworkingyears',
+   'yearsatcompany',
+   'yearsincurrentrole',
+   'yearssincelastpromotion',
+   'yearswithcurrmanager',
+   'trainingtimeslastyear'
+]
+
+cat_cols = [
+    'businesstravel', 
+    'department', 
+    'educationfield',
+    'gender', 
+    'jobrole',
+    'maritalstatus',
+]
+# https://stats.stackexchange.com/questions/430636/is-a-rating-in-a-set-range-a-categorical-or-numerical-variable#:~:text=It's%20categorical%2C%20specifically%20ordinal.,what%20you%20mean%20by%20measurement.
+#  https://stackoverflow.com/questions/29528628/how-to-specify-a-variable-in-pandas-as-ordinal-categorical
+optional_cat_cols = [
+  'education',
+  'environmentsatisfaction',
+  'jobinvolvement',
+  'joblevel',
+  'jobsatisfaction',
+  'numcompaniesworked',
+  'performancerating',
+  'stockoptionlevel',
+  'trainingtimeslastyear',
+  'worklifebalance'
+],
+binnable_cols = [
+  'yearsatcompany',
+  'yearsincurrentrole',
+  'yearssincelastpromotion',
+  'yearswithcurrmanager',
+]
+binary_cols = [
+  'overtime'
+]
+cols_to_be_dropeed = [
+  'employeecount',
+  'over18',
+  'standardhours',
+  'employeenumber'
+  ]
+
+space= {
+    'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(1)),
+    'max_depth': hp.quniform('max_depth', 5, 15, 1),
+    'n_estimators': hp.quniform('n_estimators', 5, 35, 1),
+    'num_leaves': hp.quniform('num_leaves', 5, 50, 1),
+    'boosting_type': hp.choice('boosting_type', ['gbdt', 'dart']),
+    'colsample_bytree': hp.uniform('colsample_bytree', 0.6, 1.0),
+    'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+    'num_iterations': 32,
+    'num_leaves': hp.quniform('num_leaves', 2, 20, 2),
+
+    'class_weight': hp.choice('class_weight', [None, 'balanced']),
+    'subsample_for_bin': hp.quniform('subsample_for_bin', 20000, 300000, 20000),
+    # 'feature_fraction': hp.uniform('feature_fraction', 0.5, 1),
+    'bagging_fraction': hp.uniform('bagging_fraction', 0.5, 1), #alias "subsample"
+    'min_data_in_leaf': hp.qloguniform('min_data_in_leaf', 0, 6, 1),
+    # 'lambda_l1': hp.choice('lambda_l1', [0, hp.loguniform('lambda_l1_positive', -16, 2)]),
+    # 'lambda_l2': hp.choice('lambda_l2', [0, hp.loguniform('lambda_l2_positive', -16, 2)]),
+    'verbose': -1,
+    #the LGBM parameters docs list various aliases, and the LGBM implementation seems to complain about
+    #the following not being used due to other params, so trying to silence the complaints by setting to None
+    'subsample': None, #overridden by bagging_fraction
+    # 'reg_alpha': None, #overridden by lambda_l1
+    # 'reg_lambda': None, #overridden by lambda_l2
+    'min_sum_hessian_in_leaf': None, #overrides min_child_weight
+    'min_child_samples': None, #overridden by min_data_in_leaf
+    # 'colsample_bytree': None, #overridden by feature_fraction
+#        'min_child_samples': hp.quniform('min_child_samples', 20, 500, 5),
+    'min_child_weight': hp.loguniform('min_child_weight', -16, 5), #also aliases to min_sum_hessian
+    #'colsample_bytree': hp.uniform('colsample_by_tree', 0.6, 1.0),
+}
+# ***************************************************************************
 
 def download_data():
   url = 'https://cdn.iisc.talentsprint.com/CDS/MiniProjects/hr_employee_attrition_train.csv'
@@ -37,11 +135,11 @@ def handle_outliers(df, colm):
     q1 = df.describe()[colm].loc["25%"]
     q3 = df.describe()[colm].loc["75%"]
     iqr = q3 - q1
-    lower_bound = q1 - (2 * iqr)
-    upper_bound = q3 + (2 * iqr)
+    lower_bound = q1
+    upper_bound = q3
 
-    upper_bound=df[colm].mean()+2*df[colm].std()
-    lower_bound=df[colm].mean()-2*df[colm].std()
+    # upper_bound=df[colm].mean()+2*df[colm].std()
+    # lower_bound=df[colm].mean()-2*df[colm].std()
 
     df.loc[df[colm] < lower_bound,colm] = lower_bound
     df.loc[df[colm] > upper_bound,colm] = upper_bound
@@ -57,7 +155,7 @@ def write_train_and_test_reports(X_train, X_test, y_train, y_test, rf_clf):
     st.write( accuracy_score(y_train, y_pred_train))
     st.dataframe(pd.DataFrame(classification_report(y_train, y_pred_train, output_dict=True)))
 
-def kaggle_predictions(train_model_path, cols_to_be_dropeed, cat_cols):
+def kaggle_predictions(train_model_path, out_res_name):
     clf = pickle.load(open(train_model_path, 'rb'))
     df_kgle = pd.read_csv('hr_employee_attrition_test.csv')
     df_kgle = df_kgle.drop(columns=cols_to_be_dropeed)
@@ -67,7 +165,7 @@ def kaggle_predictions(train_model_path, cols_to_be_dropeed, cat_cols):
     kaggle_pred_df = pd.DataFrame(kaggle_pred_arr, columns=['label'])
     kaggle_pred_df['id'] = list(np.arange(1, len(kaggle_pred_arr) + 1))
     kaggle_pred_df = kaggle_pred_df[['id', 'label']]
-    kaggle_pred_df.to_csv(f'submissions/xgboost_plain_balanced.csv', index=False)
+    kaggle_pred_df.to_csv(f'submissions/{out_res_name}', index=False)
 
 def main():
   df = load_data()
@@ -78,43 +176,6 @@ def main():
   st.dataframe(df.describe())
   st.write("## Box plot")
   plot_for_all_cols(df, 'box')
-
-  cat_cols = [
-    'businesstravel', 
-    'department', 
-    'educationfield',
-    'gender', 
-    'jobrole',
-    'maritalstatus',
-  ]
-  # https://stats.stackexchange.com/questions/430636/is-a-rating-in-a-set-range-a-categorical-or-numerical-variable#:~:text=It's%20categorical%2C%20specifically%20ordinal.,what%20you%20mean%20by%20measurement.
-  #  https://stackoverflow.com/questions/29528628/how-to-specify-a-variable-in-pandas-as-ordinal-categorical
-  optional_cat_cols = [
-    'education',
-    'environmentsatisfaction',
-    'jobinvolvement',
-    'joblevel',
-    'jobsatisfaction',
-    'numcompaniesworked',
-    'performancerating',
-    'stockoptionlevel',
-    'trainingtimeslastyear',
-    'worklifebalance'
-  ],
-  binnable_cols = [
-    'yearsatcompany',
-    'yearsincurrentrole',
-    'yearssincelastpromotion',
-    'yearswithcurrmanager',
-  ]
-  binary_cols = [
-    'overtime'
-  ]
-  cols_to_be_dropeed = [
-    'employeecount',
-    'over18',
-    'standardhours'
-  ]
 
   # https://www.kaggle.com/code/faressayah/ibm-hr-analytics-employee-attrition-performance#%F0%9F%93%8A-Exploratory-Data-Analysis
   fig, axis = plt.subplots(1,1, figsize=(20,20))
@@ -127,8 +188,22 @@ def main():
   sns.heatmap(df.drop(columns=non_heatmap_cols).corr(), annot=True, ax=axis)
   st.pyplot(fig)
 
+  fig, axis = plt.subplots(1,1, figsize=(20,20))
+  df.hist(figsize=(20,20), bins=50, ax=axis)
+  st.pyplot(fig)
+
   # https://machinelearningmastery.com/xgboost-for-imbalanced-classification/
   st.write("# Data Preprocessing")
+  for col in numeric_cols:
+    handle_outliers(df, col)
+  
+  st.write("## After outlier removal")
+  # fig, axis = plt.subplots(1,1, figsize=(20,20))
+  # df.hist(figsize=(20,20), bins=50, ax=axis)
+  # st.pyplot(fig)
+
+  plot_for_all_cols(df, 'box')
+  
   st.write("## Dropping less corelatted columns: ")
   st.write(cols_to_be_dropeed)
   df_final = df.drop(columns=cols_to_be_dropeed)
@@ -156,13 +231,83 @@ def main():
   X = df_cat_one_hot.drop('attrition', axis=1)
   y = df_cat_one_hot.attrition
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-  xgb_clf = XGBClassifier(scale_pos_weight=scale, seed=42)
+  xgb_clf = XGBClassifier(scale_pos_weight=scale, seed=42, num_boosting_rounds = 100, colsample_bytree = 0.7, learning_rate=0.1)
   xgb_clf.fit(X_train, y_train)
   write_train_and_test_reports(X_train, X_test, y_train, y_test, xgb_clf)
   st.write(xgb_clf.get_params())
-  pickle.dump(xgb_clf, open('models/xgboost_balanced.sav', 'wb'))
-  kaggle_predictions('models/xgboost_balanced.sav', cols_to_be_dropeed, cat_cols)
+  pickle.dump(xgb_clf, open('models/xgboost_balanced_outliers_removed_tuned.sav', 'wb'))
+  kaggle_predictions('models/xgboost_balanced_outliers_removed_tuned.sav', 'xgboost_balanced_outliers_removed_tuned.csv')
 
+  st.write('# Catboost Classifier')
+  X_cat = df_final.drop('attrition', axis = 1)
+  y_cat = df_final['attrition']
+  X_train_cat, X_test_cat, y_train_cat, y_test_cat = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+  cat_clf = CatBoostClassifier(learning_rate=0.1, n_estimators=100, random_state=42, colsample_bylevel=0.7, class_weights={0: 1, 1:6}, reg_lambda=0.2)
+  cat_clf.fit(X_train_cat, y_train_cat)
+  write_train_and_test_reports(X_train_cat, X_test_cat, y_train_cat, y_test_cat, cat_clf)
+  st.write(cat_clf.get_params())
+  pickle.dump(cat_clf, open('models/catboost_balanced_outliers_removed.sav', 'wb'))
+  kaggle_predictions('models/catboost_balanced_outliers_removed.sav', 'catboost_balanced_outliers_removed.csv')
+
+
+def hyper_opt_best_model(X_train, y_train):
+    space = { 'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(1)),
+        'colsample_bytree': hp.uniform('colsample_bytree', 0.6, 1.0),
+        'reg_lambda': hp.uniform('reg_lambda', 0.0, 1.0),
+        'max_depth': hp.quniform('max_depth', 10, 50, 10),
+        'n_estimators' : hp.choice('n_estimators', [10, 50, 100, 150, 200, 250, 300, 350, 400])
+      }
+    def objective(params):
+      int_types = [ "max_depth", "n_estimators"]
+      params = convert_int_params(int_types, params)
+      # float_types = ["min_samples_leaf", "min_samples_split"]
+      # params = convert_float_params(float_types, params)
+      params['random_state'] = 42
+      model = XGBClassifier(**params)
+    
+      accuracy = cross_val_score(model, X_train, y_train, cv = 5).mean()
+      # We aim to maximize accuracy, therefore we return it as a negative value
+      return {'loss': -accuracy, 'status': STATUS_OK }
+  
+    trials = Trials()
+    best = fmin(fn= objective,
+              space= space,
+              algo= tpe.suggest,
+              max_evals = 80,
+              trials= trials)
+                
+    return best
+
+def hyper_opt_best_xgboost():
+  df = load_data()
+  df['overtime'].replace({'Yes': 1, 'No': 0}, inplace=True)
+  df['attrition'] = df['attrition'].replace({'Yes': 1, 'No': 0})
+  for col in numeric_cols:
+      handle_outliers(df, col)
+  df_final = df.drop(columns=cols_to_be_dropeed)
+  df_cat_one_hot = pd.get_dummies(df_final, columns=cat_cols, dtype='uint8')
+  X = df_cat_one_hot.drop('attrition', axis=1)
+  y = df_cat_one_hot.attrition
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+  best_param = hyper_opt_best_model(X_train, y_train)
+  int_types = [ "max_depth", "n_estimators"]
+  best_param = convert_int_params(int_types, best_param)
+  xgb_clf = XGBClassifier(**best_param)
+ 
+  xgb_clf.fit(X_train, y_train)
+  write_train_and_test_reports(X_train, X_test, y_train, y_test, xgb_clf)
+  pickle.dump(xgb_clf, open('models/xgboost_balanced_outliers_removed_tuned_hyperopt.sav', 'wb'))
+  kaggle_predictions('models/xgboost_balanced_outliers_removed_tuned_hyperopt.sav', 'xgboost_balanced_outliers_removed_tuned_hyperopt.csv')
+  
+
+# plotting and plain xgboost and catboost
 main()
+  
+#hyperopt xgboost
+# hyper_opt_best_xgboost()
+
+#predicting using any saved model
+# kaggle_predictions('models/xgboost_balanced_outliers_removed_tuned_hyperopt.sav', 'xgboost_balanced_outliers_removed_tuned_hyperopt.csv')
+
 
 
---
+++ utils.py
@@ -0,0 +1,27 @@
+def is_number(s):
+    if s is None:
+        return False
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+#convert given set of paramaters to integer values
+#this at least cuts the excess float decimals if they are there
+def convert_int_params(names, params):
+    for int_type in names:
+        #sometimes the parameters can be choices between options or numerical values. like "log2" vs "1-10"
+        raw_val = params[int_type]
+        if is_number(raw_val):
+            params[int_type] = int(raw_val)
+    return params
+
+#convert float parameters to 3 digit precision strings
+#just for simpler diplay and all
+def convert_float_params(names, params):
+    for float_type in names:
+        raw_val = params[float_type]
+        if is_number(raw_val):
+            params[float_type] = '{:.3f}'.format(raw_val)
+    return params
